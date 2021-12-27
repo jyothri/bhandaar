ac4c83e5cefffd718790ec788fb11982f554bdd8
Jyothrilinga K <jyothri@gmail.com>
2021-12-27 13:55:22 -0800

    Initial implementation of reading from Google Drive using v3 API.

---
+++ cloud.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"strings"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/googleapi"
+	"google.golang.org/api/option"
+)
+
+// List of fields to be retreived on file resource from the drive API.
+var fields []string = []string{"size", "id", "name", "mimeType", "parents", "modifiedTime"}
+
+// Filter files list by this criteria.
+const queryString = "name contains 'tesla'"
+
+var driveService *drive.Service
+
+func init() {
+	oauth_client_id := flag.String("oauth_client_id", "dummy", "oauth client id")
+	oauth_client_secret := flag.String("oauth_client_secret", "dummy", "oauth client secret")
+	refresh_token := flag.String("refresh_token", "dummy", "refresh token for the user")
+	flag.Parse()
+
+	config := &oauth2.Config{
+		ClientID:     *oauth_client_id,
+		ClientSecret: *oauth_client_secret,
+		Endpoint:     google.Endpoint,
+		Scopes:       []string{drive.DriveReadonlyScope},
+	}
+	tokenSrc := oauth2.Token{
+		RefreshToken: *refresh_token,
+	}
+	ctx := context.Background()
+	var err error
+	driveService, err = drive.NewService(ctx, option.WithTokenSource(config.TokenSource(ctx, &tokenSrc)))
+	checkError(err)
+}
+
+func cloudDrive() {
+	fileList, err := driveService.Files.List().PageSize(10).Q(queryString).Fields(googleapi.Field(strings.Join(addPrefix(fields, "files/"), ","))).Do()
+	checkError(err)
+
+	parseInfo = make(map[string][]fileData)
+	for _, file := range fileList.Files {
+		fd := fileData{
+			FilePath:  file.Id,
+			IsDir:     file.MimeType == "application/vnd.google-apps.folder",
+			ModTime:   parseTime(file.ModifiedTime),
+			FileCount: 1,
+		}
+		if !fd.IsDir {
+			fd.Size = uint(file.Size)
+			fd.FileCount = 1
+			for _, parent := range file.Parents {
+				parentFile := getFileInfo(parent)
+				_, present := parseInfo[parentFile.Name]
+				if !present {
+					parseInfo[parentFile.Name] = append(make([]fileData, 0), fileData{FilePath: parent, IsDir: true, ModTime: parseTime(parentFile.ModifiedTime), FileCount: 0})
+				}
+				prevValues := parseInfo[parentFile.Name]
+				updatedValues := make([]fileData, 0)
+				for _, fd2 := range prevValues {
+					if fd2.FilePath != parent {
+						continue
+					}
+					fd2.FileCount += 1
+					fd2.Size += fd.Size
+					updatedValues = append(updatedValues, fd2)
+				}
+				parseInfo[parentFile.Name] = updatedValues
+			}
+			parseInfo[file.Name] = append(parseInfo[file.Name], fd)
+		} else {
+			// It is possible that directory is already captured as part of the file traversal.
+			// Insert only if the directory info is not already saved.
+			if !existsKeyAndId(parseInfo, file.Name, file.Id) {
+				parseInfo[file.Name] = append(parseInfo[file.Name], fd)
+			}
+		}
+
+	}
+}
+
+func getFileInfo(fileId string) *drive.File {
+	file, err := driveService.Files.Get(fileId).Fields(googleapi.Field(strings.Join(fields, ","))).Do()
+	checkError(err)
+	return file
+}
+
+func addPrefix(in []string, prefix string) []string {
+	out := make([]string, len(in))
+	for idx, str := range in {
+		out[idx] = prefix + str
+	}
+	return out
+}
+
+func existsKeyAndId(theMap map[string][]fileData, mapKey string, id string) bool {
+	values, present := theMap[mapKey]
+	if !present {
+		return false
+	} else {
+		for _, fd2 := range values {
+			if !fd2.IsDir {
+				continue
+			}
+			if fd2.FilePath == id {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func parseTime(inputTime string) time.Time {
+	parsedTime, err := time.Parse(time.RFC3339, inputTime)
+	checkError(err)
+	return parsedTime
+}
--- folder_stats.go
+++ folder_stats.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"encoding/gob"
 	"fmt"
-	"io/fs"
-	"log"
-	"os"
-	"path/filepath"
-	"runtime"
 	"time"
 )
 
@@ -22,121 +16,17 @@ type fileData struct {
 var parseInfo map[string][]fileData
 
 func main() {
-	// const parentDir = "C:\\Users\\jyoth\\technical\\"
-	const parentDir = "/Users/jyothri/test"
-	const saveFile = "./FolderStats.gob"
-	parseInfo = make(map[string][]fileData)
-
-	collectStats(parentDir)
-
-	fmt.Printf("Saving stats to file %v \n", saveFile)
-	saveStatsToFile(saveFile)
-
-	fmt.Printf("Loading stats from file %v \n", saveFile)
-	parseInfo = make(map[string][]fileData)
-	loadStatsFromFile(saveFile)
-
-	fmt.Println("Printing stats")
+	cloudDrive()
 	printStats()
-}
-
-// Gathers the info for the directory.
-// Returns a tuple of (size of the directory, no. of files contained)
-func collectStats(parentDir string) (int64, int64) {
-	var directorySize int64
-	var fileCount int64 = 0
-	err := filepath.Walk(parentDir, func(path string, info fs.FileInfo, err error) error {
-		checkError(err)
-		// filepath.Walk also traverses the parent directory.
-		// As we call the same function recursively, we would
-		// end up invoking with the same arg again which results
-		// in an infinite loop. This check prevents traversing
-		// the same directory again.
-		if parentDir == path {
-			return nil
-		}
-
-		// Skip hidden files and directories
-		if runtime.GOOS != "windows" && info.Name()[0:1] == "." {
-			// unix/linux file or directory that starts with . is hidden
-			return nil
-		}
-
-		fd := fileData{
-			FilePath:  path,
-			IsDir:     info.IsDir(),
-			ModTime:   info.ModTime(),
-			FileCount: 1,
-		}
-		if info.IsDir() {
-			ds, fc := collectStats(path)
-			directorySize += ds
-			fileCount += fc
-			fd.Size = uint(ds)
-			fd.FileCount = uint(fc)
-		} else {
-			directorySize += info.Size()
-			fileCount++
-			fd.Size = uint(info.Size())
-			fd.FileCount = 1
-		}
-		parseInfo[info.Name()] = append(parseInfo[info.Name()], fd)
-		// filepath.Walk works recursively. However our call to
-		// collectStats also performs the traversal recursively.
-		// Returns `filepath.SkipDir` limits to only the files and folders
-		// in current directory to prevent multiple traversals.
-		if info.IsDir() {
-			return filepath.SkipDir
-		}
-		return nil
-	})
-	checkError(err)
-	return directorySize, fileCount
+	localDrive()
 }
 
 func printStats() {
-	for _, filesData := range parseInfo {
+	for fileName, filesData := range parseInfo {
 		for _, fd := range filesData {
-			// fmt.Printf("filename: %v Path: %v Size: %v ModifiedTime: %v Directory?: %v Contained files: %v \n ", fileName, fd.FilePath, fd.Size, fd.ModTime, fd.IsDir, fd.FileCount)
-			fmt.Printf("Size: %v Path: %v Contained files: %v \n ", fd.Size, fd.FilePath, fd.FileCount)
+			fmt.Printf("fileName: %30.30v Path: %v Size: %v ModifiedTime: %v Directory?: %v Contained files: %v \n ", fileName, fd.FilePath, fd.Size, fd.ModTime, fd.IsDir, fd.FileCount)
 		}
 	}
-}
-
-func saveStatsToFile(saveFile string) {
-	gobFile, err := os.Create(saveFile)
-	checkError(err)
-	defer gobFile.Close()
-
-	encoder := gob.NewEncoder(gobFile)
-
-	// Encoding the data
-	err = encoder.Encode(parseInfo)
-	checkError(err)
-	err = gobFile.Close()
-	checkError(err)
-}
-
-func loadStatsFromFile(saveFile string) {
-	_, err := os.Stat(saveFile)
-	if err != nil {
-		if os.IsNotExist(err) {
-			log.Fatal("File does not exist.")
-		}
-		panic(err)
-	}
-
-	gobFile, err := os.Open(saveFile)
-	checkError(err)
-	defer gobFile.Close()
-
-	decoder := gob.NewDecoder(gobFile)
-
-	// Decoding the data
-	err = decoder.Decode(&parseInfo)
-	checkError(err)
-	err = gobFile.Close()
-	checkError(err)
 }
 
 func checkError(err error) {
