f83d11fb29e2b263a243cf14455cfaa3fdc90d7f
Jyothrilinga Rao <jyothri@users.noreply.github.com>
2025-03-10 00:20:58 -0700

    Update ui to swtich be url from /oauth/glink to /api/glink

--- ui/src/routes/oauth/glink.tsx
+++ ui/src/routes/oauth/glink.tsx
@@ -3,6 +3,8 @@ import { backend_url } from "../../api";
 
 type oauthCode = {
   code: string;
+  state: string;
+  scope: string;
 };
 
 export const Route = createFileRoute("/oauth/glink")({
@@ -11,14 +13,17 @@ export const Route = createFileRoute("/oauth/glink")({
     // validate and parse the search params into a typed state
     return {
       code: (search.code as string) || "",
+      state: (search.state as string) || "",
+      scope: (search.scope as string) || "",
     };
   },
 });
 
 function RouteComponent() {
   const { code } = Route.useSearch();
-  const redirectUri = `${window.location.protocol}//${window.location.host}/oauth/glink`;
-  const url = `${backend_url}/oauth/glink?code=${code}&redirectUri=${redirectUri}`;
+  const redirectUri = `${window.location.protocol}//${window.location.host}/api/glink`;
+  const url = 
+    `${backend_url}/oauth/glink?code=${code}&redirectUri=${redirectUri}&state=${state}&scope=${scope}`;
   window.location.href = url;
   return (
     <div>
